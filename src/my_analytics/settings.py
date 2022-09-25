68dd1eab102140ec9827a083ef26b2d0a3edbe10
Eugenio <lugezz@gmail.com>
2022-09-25 11:00:15 -0300

    App started

--- src/my_analytics/settings.py
+++ src/my_analytics/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Own apps
+    'customers',
+    'products',
+    'profiles',
+    'reports',
+    'sales',
 ]
 
 MIDDLEWARE = [
